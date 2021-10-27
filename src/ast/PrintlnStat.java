0f94c4ca7dc3e9ce85740f72cc8ff56b182a77eb
Arthur Fernando <arthur.fernando03@gmail.com>
2021-10-27 01:32:17 -0300

    atualizacao printlnstat

--- src/ast/PrintlnStat.java
+++ src/ast/PrintlnStat.java
@@ -17,51 +17,60 @@ public class PrintlnStat extends Stat{
     pw.print("printf(\"");
     
 	if(expr.getClass() == CompositeExpr.class) {
-		if(((CompositeExpr) expr).getLeft().getType().equals(Type.intType)) {
-			pw.print("%d ");
-//			expr.genC(pw);
-		}else
-		if(((CompositeExpr) expr).getLeft().getType().equals(Type.booleanType)) {
-			pw.print("%d ");
-//			expr.genC(pw);
-//			pw.print("\"");
-			
-		}else
-		if(((CompositeExpr) expr).getLeft().getType().equals(Type.stringType)) {
-			pw.print("%s ");
-			expr.genC(pw);
+		CompositeExpr cexpr = (CompositeExpr)expr;
+		if(cexpr.getClass() == CompositeExpr.class) {
+			cexpr = (CompositeExpr)cexpr.getLeft();
 		}
-		
-		if(((CompositeExpr) expr).getRight().getType().equals(Type.intType)) {
-			pw.print("%d\", ");
-			expr.genC(pw);
+//		pw.println(cexpr.getLeft().getType());
+		if(cexpr.getLeft().getType().equals(Type.intType)) {
+			pw.print("%d ");
+			cexpr.getLeft().genC(pw);
+//			cexpr.getRight().genC(pw);
+//			pw.println(cexpr.getLeft());
+//			pw.println(cexpr.getRight());
+		}
+		else
+		if(cexpr.getLeft().getType().equals(Type.booleanType)) {
+			pw.print("%d ");
+			cexpr.getLeft().genC(pw);
+//			pw.print("\"");
+//			
 		}else
-		if(((CompositeExpr) expr).getRight().getType().equals(Type.booleanType)) {
-			expr.genC(pw);
+		if(cexpr.getLeft().getType().equals(Type.stringType)) {
+			pw.print("%s ");
+			cexpr.getLeft().genC(pw);
+		}
+//		
+		if(cexpr.getRight().getType().equals(Type.intType)) {
+			pw.print("%d\", ");
+			cexpr.getRight().genC(pw);
+		}else
+		if(cexpr.getRight().getType().equals(Type.booleanType)) {
+			cexpr.getRight().genC(pw);
 			pw.print("\",");
 			
 		}else
-		if(((CompositeExpr) expr).getRight().getType().equals(Type.stringType)) {
+		if(cexpr.getRight().getType().equals(Type.stringType)) {
 			pw.print("%s\", ");
-			expr.genC(pw);
+			cexpr.getRight().genC(pw);
 		}
 		
 //		pw.print((CompositeExpr)expr.getLeft())
 	} else {
 		
-	if(expr.getType().equals(Type.intType)) {
-		pw.print("%d\", ");
-		expr.genC(pw);
-	} else	
-	if(expr.getType().equals(Type.booleanType)) {
-		expr.genC(pw);
-		pw.print("\"");
-		
-	} else	
-	if(expr.getType().equals(Type.stringType)) {
-		pw.print("%s\", ");
-		expr.genC(pw);
-	}
+		if(expr.getType().equals(Type.intType)) {
+			pw.print("%d\", ");
+			expr.genC(pw);
+		} else	
+		if(expr.getType().equals(Type.booleanType)) {
+			expr.genC(pw);
+			pw.print("\"");
+			
+		} else	
+		if(expr.getType().equals(Type.stringType)) {
+			pw.print("%s\", ");
+			expr.genC(pw);
+		}
 	}
 //    expr.genC(pw);
     pw.println(");");
